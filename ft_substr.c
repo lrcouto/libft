2ab0bbb5c0af736c2a94fa6301f0b6ddd3eac9d1
L. R. Couto <57910428+lrcouto@users.noreply.github.com>
2020-05-21 18:32:36 -0300

    Fixing possibly faulty malloc?

--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lcouto <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: lcouto <lcouto@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/28 15:59:21 by lcouto            #+#    #+#             */
-/*   Updated: 2020/01/29 12:35:02 by lcouto           ###   ########.fr       */
+/*   Updated: 2020/05/21 18:31:40 by lcouto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 
 	i = 0;
-	if (!s || !(substr = malloc((sizeof(char) * len) + 1)))
+	if (!s || !(substr = ft_calloc(len + 1, (sizeof(char) + 1))))
 		return (NULL);
 	while (s && i < len && (start + i < ft_strlen(s)))
 	{
